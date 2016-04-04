5452ae3245622be1855acd0a22666e498b1c9e14
Brian <blamm9@gmail.com>
2016-04-04 15:25:10 -0700

    Ajjkkkjjjjjjjjjjjjjkklijkd A B B B B B B B B B B B B B B B B B B B B B B B B A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B A A A A A A A B B B A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B B

---
+++ calband_transition.m
@@ -0,0 +1,15 @@
+function [instructions] = calband_transition(initial_formation,target_formation,max_beats)
+% main function
+tag_initialform = tagInitialForm(initial_formation);
+nb = sum(initial_formation);
+for tag = 1:nb
+    [i_initial,j_initial] = find(tag_initialform == tag);
+    [i_target,j_target] = find(tag_targetform == tag);
+    if i_initial==i_target && j_initial==j_target % case when no move is needed
+        direction = '.';
+        wait = max_beats;
+    end
+    instructions(tag).i_target = i_target;
+    instructions(tag).j_target = j_target;
+    instructions(
+end
---
+++ tagInitialForm.m
@@ -0,0 +1,10 @@
+function [ tag_initialform ] = tagInitialForm(initial_formation)
+% assigns tags to marchers in initial_formation
+tag_initialform = initial_formation;
+tag = 1;
+for i = numel(initial_formation)
+    if tag_initialform(i) == 1
+        tag_initialform(i) = tag;
+        tag = tag + 1;
+    end
+end
