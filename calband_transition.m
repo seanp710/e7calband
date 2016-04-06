f1eefb9ea9fcb0a88c3e0fd3a6313ed2d15237b2
Brian <blamm9@gmail.com>
2016-04-05 23:09:24 -0700

    added stuff

--- calband_transition.m
+++ calband_transition.m
@@ -3,16 +3,21 @@ function [instructions] = calband_transition(initial_formation,target_formation,
 tag_initialform = tagFormation(initial_formation);
 tag_targetform = tagFormation(target_formation);
 nb = sum(sum(initial_formation));
+
 for tag = 1:nb
     [i_initial,j_initial] = find(tag_initialform == tag); % finds initial coords of marcher
     [i_target,j_target] = find(tag_targetform == tag); % finds final coords
     dRow = i_target - i_initial;
     dCol = j_target - j_initial
-    if i_initial==i_target && j_initial==j_target % case when no move is needed
-        direction = '.';
-        wait = max_beats;
+    if dRow==0 && d_Col==0 % case when no move is needed
+        direction = [direction '.'];
+        wait = [wait max_beats];
     else % case when move needed
-        
+        switch dRow
+            case dRow > 0
+                
+            case dRow < 0
+        end
         
     end
     % 
