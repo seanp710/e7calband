ffa7ab0975dd15382b6c25251002e84bc656a737
Brian <blamm9@gmail.com>
2016-04-05 22:59:42 -0700

    added stuff

--- calband_transition.m
+++ calband_transition.m
@@ -2,11 +2,12 @@ function [instructions] = calband_transition(initial_formation,target_formation,
 % main function
 tag_initialform = tagFormation(initial_formation);
 tag_targetform = tagFormation(target_formation);
-nb = sum(initial_formation);
+nb = sum(sum(initial_formation));
 for tag = 1:nb
-    [i_initial,j_initial] = find(tag_initialform == tag);
-    [i_target,j_target] = find(tag_targetform == tag);
-    dx = 
+    [i_initial,j_initial] = find(tag_initialform == tag); % finds initial coords of marcher
+    [i_target,j_target] = find(tag_targetform == tag); % finds final coords
+    dRow = i_target - i_initial;
+    dCol = j_target - j_initial
     if i_initial==i_target && j_initial==j_target % case when no move is needed
         direction = '.';
         wait = max_beats;
@@ -14,7 +15,8 @@ for tag = 1:nb
         
         
     end
+    % 
     instructions(tag).i_target = i_target;
     instructions(tag).j_target = j_target;
-    instructions(tag
+    instructions(tag).
 end
