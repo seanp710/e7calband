e1cb91acb8108b1ad4baa36048696ccc94f7f128
Brian <blamm9@gmail.com>
2016-04-06 00:28:41 -0700

    updated

--- tagFormation.m
+++ tagFormation.m
@@ -1,8 +1,8 @@
-function [ tag_form ] = tagFormation(initial_formation)
+function [ tag_form ] = tagFormation(initial_formation, target_formation)
 % assigns tags to marchers in initial_formation stuff
-tag_form = initial_formation;
+tag_form = target_formation;
 tag = 1;
-for i = 1:numel(initial_formation)
+for i = 1:numel(target_formation)
     if tag_form(i) == 1
         tag_form(i) = tag;
         tag = tag + 1;
