a3c46c035d0bd8d82058cf718969809c05f2e6bc
Brian <blamm9@gmail.com>
2016-04-05 22:44:31 -0700

    finished tagging

--- calband_transition.m
+++ calband_transition.m
@@ -6,11 +6,13 @@ nb = sum(initial_formation);
 for tag = 1:nb
     [i_initial,j_initial] = find(tag_initialform == tag);
     [i_target,j_target] = find(tag_targetform == tag);
+    dx = 
     if i_initial==i_target && j_initial==j_target % case when no move is needed
         direction = '.';
         wait = max_beats;
-    else
-        % case when move needed
+    else % case when move needed
+        
+        
     end
     instructions(tag).i_target = i_target;
     instructions(tag).j_target = j_target;
--- tagFormation.m
+++ tagFormation.m
@@ -2,7 +2,6 @@ function [ tag_form ] = tagFormation(initial_formation)
 % assigns tags to marchers in initial_formation stuff
 tag_form = initial_formation;
 tag = 1;
-numel(initial_formation)
 for i = 1:numel(initial_formation)
     if tag_form(i) == 1
         tag_form(i) = tag;
